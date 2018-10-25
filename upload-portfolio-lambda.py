87c63981aee84ae822beca727198ef142fdc755b
Hoogenboom <wes.hoogenboom@gmail.com>
2018-10-25 15:53:04 -0400

    Use SNS to notify when portfolio is deployed

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -2,22 +2,31 @@ import boto3
 from botocore.client import Config
 import StringIO
 import zipfile
+import mimetypes
 
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
-    
-    portfolio_bucket = s3.Bucket('portfolio.weshoogenboom.com')
-    build_bucket = s3.Bucket('portfoliobuild.weshoogenboom.com')
-    
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
-    
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm)
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:539127747096:deployPortfolioTopic')
 
-    print "Job Done!"
+    try:
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
+        portfolio_bucket = s3.Bucket('portfolio.weshoogenboom.com')
+        build_bucket = s3.Bucket('portfoliobuild.weshoogenboom.com')
+
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm,
+                 ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+        print "Job Done!"
+        topic.publish(Subject="Portfolio Deployed", Message="Portfolio Deployed successfully")
+    except:
+        topic.publish(Subject="Portfolio Deploy Failed", Message="The portfolio was not deployed successfully!")
+        raise
     return 'Hello from Lambda'
